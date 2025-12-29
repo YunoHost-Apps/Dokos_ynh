e82ac5a2d9068426aa47146c20744b38259e9e1e
Thomas <51749973+Thovi98@users.noreply.github.com>
2025-12-29 20:55:13 +0100

    convert to helpers2.1

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
 #=================================================
-# COMMON VARIABLES
+# COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
-nodejs_version=18
 
-#=================================================
-# PERSONAL HELPERS
-#=================================================
 install_wkhtmltopdf_patched_qt_package() {
     # Create the temporary directory
 	tempdir="$(mktemp -d)"
 	# Download the deb files
 	ynh_setup_source --dest_dir="$tempdir" --source_id="wkhtmltopdf_patched_qt"
     # Install the package
-    ynh_package_install \
+    _ynh_apt_install \
         "$tempdir/wkhtmltopdf_patched_qt.deb"
     # Mark packages as dependencies, to allow automatic removal
     apt-mark auto wkhtmltopdf
@@ -22,11 +18,11 @@ install_wkhtmltopdf_patched_qt_package() {
 
 uninstall_wkhtmltopdf_patched_qt_package() {
     apt-mark unhold wkhtmltopdf
-    ynh_package_autopurge wkhtmltopdf
+    _ynh_apt autoremove --purge wkhtmltopdf
 }
 
 install_app_to_bench() {
-    
+
     local apps_dir=$install_dir/dokos-bench-folder/apps
 
     if [ $1 == "erpnext" ]; then
@@ -37,30 +33,31 @@ install_app_to_bench() {
     ynh_setup_source --dest_dir="$apps_dir/$1" --source_id="$src"
 
     chmod u+x $install_dir/dokos-bench-folder/env/bin/activate
-    ynh_exec_warn_less ynh_exec_as $app $install_dir/dokos-bench-folder/env/bin/activate
-    ynh_exec_warn_less ynh_exec_as $app $install_dir/dokos-bench-folder/env/bin/python -m pip install --upgrade -e $apps_dir/$1
+    ynh_hide_warnings ynh_exec_as_app $install_dir/dokos-bench-folder/env/bin/activate
+    ynh_hide_warnings ynh_exec_as_app $install_dir/dokos-bench-folder/env/bin/python -m pip install --upgrade -e $apps_dir/$1
 
     chown -R $app: "$apps_dir/$1"
 
     local apps_list=$install_dir/dokos-bench-folder/sites/apps.txt
     if test -f "$apps_list"; then
-        ynh_exec_warn_less ynh_exec_as $app echo -e "\n$1" >> $apps_list
-    else 
-        ynh_exec_warn_less ynh_exec_as $app echo "$1" >> $apps_list
+        ynh_hide_warnings ynh_exec_as_app echo -e "\n$1" >> $apps_list
+    else
+
+        ynh_hide_warnings ynh_exec_as_app echo "$1" >> $apps_list
     fi
 
     pushd $apps_dir/$1
-        ynh_use_nodejs
-        ynh_exec_warn_less ynh_exec_as $app env $ynh_node_load_PATH yarn install --check-files
+
+        ynh_hide_warnings ynh_exec_as_app yarn install --check-files
 
         # Builds assets for the Frappe Applications installed on bench
-        ynh_exec_warn_less ynh_exec_as $app env PATH=$install_dir/bin:$PATH bench build --app $1
+        ynh_hide_warnings ynh_exec_as_app PATH=$install_dir/bin:$PATH bench build --app $1
     popd
 
 }
 
 upgrade_app_to_bench() {
-    
+
     local apps_dir=$install_dir/dokos-bench-folder/apps
 
     if [ $1 == "erpnext" ]; then
@@ -72,17 +69,17 @@ upgrade_app_to_bench() {
     ynh_setup_source --dest_dir="$apps_dir/$1" --source_id="$1" --full_replace
 
     chmod u+x $install_dir/dokos-bench-folder/env/bin/activate
-    ynh_exec_warn_less ynh_exec_as $app $install_dir/dokos-bench-folder/env/bin/activate
-    ynh_exec_warn_less ynh_exec_as $app $install_dir/dokos-bench-folder/env/bin/python -m pip install --upgrade -e $apps_dir/$1
+    ynh_hide_warnings ynh_exec_as_app $install_dir/dokos-bench-folder/env/bin/activate
+    ynh_hide_warnings ynh_exec_as_app $install_dir/dokos-bench-folder/env/bin/python -m pip install --upgrade -e $apps_dir/$1
 
     chown $app: -R "$apps_dir/$1"
 
     pushd $apps_dir/$1
-        ynh_use_nodejs
-        ynh_exec_warn_less ynh_exec_as $app env $ynh_node_load_PATH yarn install --check-files
-        
+
+        ynh_hide_warnings ynh_exec_as_app yarn install --check-files
+
         # Builds assets for the Frappe Applications installed on bench
-        ynh_exec_warn_less ynh_exec_as $app env PATH=$install_dir/bin:$PATH bench build --app $1
+        ynh_hide_warnings ynh_exec_as_app PATH=$install_dir/bin:$PATH bench build --app $1
     popd
 
 }
